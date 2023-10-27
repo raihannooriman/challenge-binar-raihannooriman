ced3fe91c1fe4a3b22b02bbb87d7a6b704fc929f
noorimanr@gmail.com <noorimanr@gmail.com>
2023-10-27 20:57:22 +0800

    update

--- Challenge_4/server/index.js
+++ Challenge_4/server/index.js
@@ -1,6 +1,32 @@
-const server = http.createServer(router);
+const http = require("http");
+const port = 8000;
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+const fs = require("fs");
+
+http
+.createServer((req, res) => {
+    switch (req.url) {
+        case "/":
+        req.url = "index.html";
+        break;
+        case "/cars":
+        req.url = "sewa.html";
+        break;
+    }
+    let path = "public/" + req.url;
+    fs.readFile(path, (err, data) => {
+        res.writeHead(200);
+        res.end(data);
+    });
+})
+
+  // //Error
+  // .listen(port, '0.0.0.0', () => {
+  //   console.log("server berjalan di http://0.0.0.0:%d", port)
+  // })
+
+  //Success
+.listen(port, "localhost", () => {
+    console.log(
+        "Server sudah berjalan, silahkan buka http://localhost:%d",port);
 });
